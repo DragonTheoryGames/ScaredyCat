06f7b6fc67490025a75570ca54ca11432a3fcc87
Cody Fulfer <Codeman1010@gmail.com>
2020-01-22 23:07:40 -0700

    Kitty Runs Yay!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,18 +8,31 @@ public class PlayerController : MonoBehaviour {
     [SerializeField]
     private Rigidbody2D myRB;
     [SerializeField]
-    private isGrounded isGrounded;
+    private Transform myTransform;
+    [SerializeField]
+    private Collider2D myCollider;
+    [SerializeField]
+    private Animator animator;
 
-    private float moveSpeed = 15f;
+    private bool isGrounded;
+    [SerializeField]
+    private float moveSpeed = 900f;
     private float jumpSpeed = 15f;
 
     void FixedUpdate() {
-        PlayerMove();
+        PlayerRun();
         PlayerJump();
     }
 
-    private void PlayerMove() {
-        float x_Move = Input.GetAxis("Horizontal") * moveSpeed;
+    private void PlayerRun() {
+        float x_Move = (Input.GetAxis("Horizontal") * moveSpeed) * Time.fixedDeltaTime;
+        if (Mathf.Abs(x_Move) != 0){ //turns player
+            myTransform.localScale = new Vector3(Mathf.Sign(x_Move), 1, 1);
+            animator.SetBool("isRunning", true);
+        }
+        else{
+            animator.SetBool("isRunning", false);
+        }
         myRB.velocity = new Vector2(x_Move, myRB.velocity.y);
     }
 
@@ -28,6 +41,8 @@ public class PlayerController : MonoBehaviour {
             myRB.velocity = new Vector2(myRB.velocity.x, jumpSpeed);
         }
     }
+
+
 
     
 }
